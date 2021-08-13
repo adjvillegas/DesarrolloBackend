75cda371ba56f87af7498461e7c328797b8e174b
adjvillegas <adj.villegas@gmail.com>
2021-08-12 22:29:52 -0300

    agregados desafio 8

--- Desafio8 VillegasJavier/app.js
+++ Desafio8 VillegasJavier/app.js
@@ -1,25 +1,38 @@
 const express = require('express');
 
-const items = {}
+const Items = require('./itemsClass/items');
+const items = new Items();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 const server = app.listen(8080, () => {
     console.log("Escuchando en el puerto 8080")
-} );
+});
 
-
+// Todo se responde en formato JSON --> Debe andar en POSTMAN
+//    Devuelve array de productos --> Si no existen devuelve error
 app.get('/api/productos/listar', (req, res) => {
 
-})
+    if (items.isHasExist())
+        res.json({error: 'no hay productos cargados'})
 
+    res.json(items)
+
+});
+
+// Lista en forma individual --> Si no existe devuelve error
 app.get('/api/productos/listar/:id', (req, res) => {
 
-})
+});
 
+
+// Almacena un producto --> Devuelve el producto guardado
 app.post('api/productos/guardar/', (req, res) => {
 
-})
+});
 
 
-server.on("error", err => console.error(`Error en servidor ${error}`))
+server.on("error", err => console.error(`Error en servidor ${error}`));
---
+++ Desafio8 VillegasJavier/itemsClass/items.js
@@ -0,0 +1,11 @@
+class Items {
+    constructor () {
+        this.Items = []
+    }
+
+    isHasExist = () => {
+        return (this.Items > 0)
+    }
+}
+
+module.exports = Items;
