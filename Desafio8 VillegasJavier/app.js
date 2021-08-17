35f078eebaf8f0fe9d434fdf8f4821409d4283c9
adjvillegas <adj.villegas@gmail.com>
2021-08-16 23:02:43 -0300

    desafio8

--- Desafio8 VillegasJavier/app.js
+++ Desafio8 VillegasJavier/app.js
@@ -6,6 +6,7 @@ const items = new Items();
 const app = express();
 
 app.use(express.json());
+app.use(express.text());
 app.use(express.urlencoded({extended: true}))
 
 const server = app.listen(8080, () => {
@@ -16,21 +17,40 @@ const server = app.listen(8080, () => {
 //    Devuelve array de productos --> Si no existen devuelve error
 app.get('/api/productos/listar', (req, res) => {
 
-    if (items.isHasExist())
-        res.json({error: 'no hay productos cargados'})
+    // if (!items.isHasExist())
+    //     res.json({error: 'no hay productos cargados'})
 
-    res.json(items)
+    // res.json(items)
+
+    items.guardar(
+        "hola",
+        123,
+        "q12312312"
+    )
+
+    res.json(items.Items)
 
 });
 
 // Lista en forma individual --> Si no existe devuelve error
 app.get('/api/productos/listar/:id', (req, res) => {
 
+    let listar = items.listar(req.params.id)
+
+    res.json(listar)
+
 });
 
 
 // Almacena un producto --> Devuelve el producto guardado
-app.post('api/productos/guardar/', (req, res) => {
+app.post('/api/productos/guardar', (req, res) => {
+
+        if ( req.body.title !== undefined && req.body.price !== undefined && req.body.thumbnail !== undefined) {
+        
+            items.guardar(req.body.title, req.body.price, req.body.thumbnail)
+
+        res.json({title: req.body.title, price: req.body.price, thumbnail: req.body.thumbnail})
+        } else res.json({error: 'Error al guardar'})
 
 });
 
--- Desafio8 VillegasJavier/itemsClass/items.js
+++ Desafio8 VillegasJavier/itemsClass/items.js
@@ -1,10 +1,29 @@
 class Items {
     constructor () {
-        this.Items = []
+        this.Items = [];
     }
 
     isHasExist = () => {
         return (this.Items > 0)
+    }
+
+    listar = (id) => {
+    }
+
+    guardar = (title, price, thumbnail) => {
+
+        // if (!this.isHasExist) {
+            this.Items.push({"title": title, "price": price, "thumbnail": thumbnail})
+        // } else {
+            // this.Items = [{
+            //     "title": title, 
+            //     "price": price, 
+            //     "thumbnail": thumbnail
+            // }]
+        }
+
+            
+
     }
 }
 
