a0bfac9c2782eab771022448c13938537e273c09
adjvillegas <adj.villegas@gmail.com>
2021-08-25 22:03:42 -0300

    Inicio desafio 11

---
+++ Desafio11/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+const Items = require('./itemsClass/items');
+const items = new Items();
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+    res.render('index.pug', {message: 'Bienvenido al desafio 11 con PUG'})
+})
+
+app.get('/productos/vista', (req, res) => {
+
+    res.render('index.pug', {message: 'Visualizar Producto', products: items.Items, view: 'get'} ) 
+
+})
+
+app.get('/productos/guardar', (req, res) => {
+    res.render('index.pug', {view: 'post'})
+})
+
+app.post('/productos/guardar', (req, res) => {
+    let { title, price, thumbnail } = req.body;
+
+    if ( title !== undefined && 
+        price !== undefined && 
+        thumbnail !== undefined) {
+   
+       items.guardar(title, price, thumbnail);
+
+       res.render('index.pug', {message: `Guardaste Algo ${title} ${price} ${thumbnail}`})
+
+   } else     res.render('index.pug', {message: `No pudiste guardar ${title} ${price} ${thumbnail}`}) 
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el puerto ${PORT}`)
+})
+
